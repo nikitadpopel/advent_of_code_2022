6ccb883779361463fcc647d3c0049c512ef7c533
nikitadpopel <nikita.d.popel@gmail.com>
2022-12-12 20:34:00 -0500

    will do day12 tomorrow

---
+++ python/day12.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('../')
+from helper_functions import *
+import time
+
+
+def part1(lineList):
+    pass
+
+def part2(lineList):
+    pass
+
+if __name__ == '__main__':
+    lineList = readInput('../inputs/day12.txt')
+    print(part1(lineList))
+    print(part2(lineList))
