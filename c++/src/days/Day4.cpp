a35c1d52c287b8700303c5893c080243661f8df0
nikitadpopel <nikita.d.popel@gmail.com>
2022-12-24 19:09:33 -0500

    added day4 c++

--- c++/src/days/Day4.cpp
+++ c++/src/days/Day4.cpp
@@ -66,6 +66,88 @@ int Day4::Part1(std::vector<std::string> sectionlist)
 		}
 		
 		std::vector<int>::iterator iter2;
+		int findcount = 0;
+		for(iter2 = section1.begin(); iter2 < section1.end(); iter2++ )
+		{
+			if(std::find(section2.begin(), section2.end(), *iter2) != section2.end())
+			{
+				findcount += 1;
+			}
+		}
+		if(findcount == section1.size())
+		{
+			totalcount += 1;
+		}
+
+		findcount = 0;
+		for(iter2 = section2.begin(); iter2 < section2.end(); iter2++ )
+		{
+			if(std::find(section1.begin(), section1.end(), *iter2) != section1.end())
+			{
+				findcount += 1;
+			}
+		}
+		if(findcount == section2.size())
+		{
+			totalcount += 1;
+		}
+	}
+	return totalcount;
+}
+
+int Day4::Part2 (std::vector<std::string> sectionlist)
+{
+    std::vector<std::string>::iterator iter;
+	int totalcount = 0;
+	for(iter = sectionlist.begin(); iter < sectionlist.end(); iter++ )
+	{   
+		std::string sections = *iter;
+		int sec1[2] = {0,0};
+		int sec2[2] = {0,0};
+		sec1[0] = stoi(sections.substr(0, sections.find("-")));
+		sections = sections.substr(sections.find("-") + 1, sections.length());
+		sec1[1] = stoi(sections.substr(0, sections.find(",")));
+		sections = sections.substr(sections.find(",") + 1, sections.length());
+		sec2[0] = stoi(sections.substr(0, sections.find("-")));
+		sections = sections.substr(sections.find("-") + 1, sections.length());
+		sec2[1] = stoi(sections.substr(0, sections.find(",")));
+		sections = sections.substr(sections.find(",") + 1, sections.length());
+
+		std::vector<int> section1;
+		std::vector<int> section2;
+		int tempmin = 0;
+		int tempmax = 0;
+		if(sec1[0]>sec1[1])
+		{
+			tempmin = sec1[1];
+			tempmax = sec1[0];
+		}
+		else
+		{
+			tempmin = sec1[0];
+			tempmax = sec1[1];
+		}
+		for(int i = tempmin; i <= tempmax; i++)
+		{
+			section1.push_back(i);
+		}
+		
+		if(sec2[0]>sec2[1])
+		{
+			tempmin = sec2[1];
+			tempmax = sec2[0];
+		}
+		else
+		{
+			tempmin = sec2[0];
+			tempmax = sec2[1];
+		}
+		for(int i = tempmin; i <= tempmax; i++)
+		{
+			section2.push_back(i);
+		}
+		
+		std::vector<int>::iterator iter2;
 		bool isfound = false;
 		for(iter2 = section1.begin(); iter2 < section1.end(); iter2++ )
 		{
@@ -81,17 +163,6 @@ int Day4::Part1(std::vector<std::string> sectionlist)
 		}
 	}
 	return totalcount;
-}
-
-int Day4::Part2 (std::vector<std::string> sectionlist)
-{
-    std::vector<std::string>::iterator iter;
-	int totalcount = 0;
-	for(iter = sectionlist.begin(); iter < sectionlist.end(); iter++ )
-	{   
-	}
-	
-    return totalcount;
 }
 
 bool Day4::Solve()
