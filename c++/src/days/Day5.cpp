fc0886ffca4206d9c9525454652dbb8edb7c5db6
nikitadpopel <nikita.d.popel@gmail.com>
2022-12-24 20:08:15 -0500

    added progress

--- c++/src/days/Day5.cpp
+++ c++/src/days/Day5.cpp
@@ -9,9 +9,19 @@ Day5::Day5()
 {
 	//m_vecInput = ReadInput("../../../inputs/day5.txt");
 	m_vecInput = ReadInput("../inputs/day5.txt");
+	ParseInput();
+	PrintCrates();
 }
 
+void Day5::ParseInput()
+{
 
+}
+
+void Day5::PrintCrates()
+{
+	std::cout << "Crates:" <<std::endl;
+}
 
 int Day5::Part1(std::vector<std::string> moves)
 {
@@ -35,6 +45,7 @@ int Day5::Part2 (std::vector<std::string> moves)
 
 bool Day5::Solve()
 {
+
 	std::cout << Part1(m_vecInput) <<std::endl;
 	std::cout << Part2(m_vecInput) <<std::endl;
 	return true;
--- c++/src/days/Day5.h
+++ c++/src/days/Day5.h
@@ -5,9 +5,13 @@ class Day5
 {
     public:
         Day5();
+        void ParseInput();
+        void PrintCrates();
         int Part1(std::vector<std::string> moves);
         int Part2(std::vector<std::string> moves);
         bool Solve();
     private:
         std::vector<std::string> m_vecInput;
+        std::vector<std::string> m_moves;
+        std::vector<std::vector<char>> m_crates;
 };
